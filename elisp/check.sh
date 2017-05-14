c93baa353c70d0c860d3ccf855b6d8ca43e928bf
Steve Purcell <steve@sanityinc.com>
2017-05-14 14:38:48 +1200

    Make check.sh for elisp work correctly

--- elisp/check.sh
+++ elisp/check.sh
@@ -8,11 +8,17 @@ INIT_PACKAGE_EL="(progn
   (package-initialize)
   (package-refresh-contents))"
 
-cd $ELDIR
+cd "$ELDIR"
+echo '*** INSTALLING ELISP PREREQUISITES'
+cask && echo OK
 
+echo
+echo '*** CHECKING ELISP BYTE-COMPILES CLEANLY'
 cask emacs -Q --eval "(setq byte-compile-error-on-warn t)" \
-     -batch -f batch-byte-compile *.el
+     -batch -f batch-byte-compile ./*.el && echo OK
 
-cask emacs -q --eval "(require 'package-lint) $INIT_PACKAGE_EL" \
-     -batch -f package-lint-batch-and-exit intero.el
+echo
+echo '*** CHECKING ELISP FOR PACKAGING ISSUES'
+cask emacs -Q --eval "$INIT_PACKAGE_EL" \
+     -batch -l package-lint -f package-lint-batch-and-exit intero.el && echo OK
 
