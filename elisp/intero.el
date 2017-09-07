153abdfb327e023a29ef62eb35098470cb8a4f26
Steve Purcell <steve@sanityinc.com>
2017-09-07 20:33:49 +1200

    Restore C-g binding in intero-multiswitch
    See #469

--- elisp/intero.el
+++ elisp/intero.el
@@ -2419,6 +2419,7 @@ Each option is a plist of (:key :default :title) wherein:
            (let ((map (copy-keymap widget-keymap)))
              (define-key map (kbd "C-c C-c") 'exit-recursive-edit)
              (define-key map (kbd "C-c C-k") 'abort-recursive-edit)
+             (define-key map (kbd "C-g") 'abort-recursive-edit)
              map))
           (widget-setup)
           (recursive-edit)
