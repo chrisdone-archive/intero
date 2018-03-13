ebcf8a0935b57eb54351b78ed8c358cbef96f4bd
Chris Done <chrisdone@gmail.com>
2018-03-13 13:05:28 +0000

    Update elisp to 0.1.30

--- elisp/intero.el
+++ elisp/intero.el
@@ -74,7 +74,7 @@
     ;; Until <https://github.com/haskell/network/issues/313> is fixed:
     (windows-nt "0.1.28")
     (cygwin "0.1.28")
-    (t "0.1.29"))
+    (t "0.1.30"))
   "Package version to auto-install.
 
 This version does not necessarily have to be the latest version
