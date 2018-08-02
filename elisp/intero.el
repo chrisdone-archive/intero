2f1002e3a1c0fa98ae270287f53d359ea96f7093
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2018-08-02 15:23:35 +0100

    Return the buffer instead of the process

--- elisp/intero.el
+++ elisp/intero.el
@@ -1342,8 +1342,8 @@ STORE-PREVIOUS is non-nil, note the caller's buffer in
          (backend-buffer (intero-buffer 'backend)))
     (with-current-buffer
         (or (let ((buf (get-buffer name)))
-              (and buf
-                   (get-buffer-process buf)))
+              (and (get-buffer-process buf)
+                   buf))
             (with-current-buffer
                 (get-buffer-create name)
               ;; The new buffer doesn't know if the initial buffer was hosted
